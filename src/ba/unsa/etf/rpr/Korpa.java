80de5a59202acaf39d4349c3d7fdd4594b58f89c
epiljug1 <epiljug1@etf.unsa.ba>
2020-10-29 20:38:04 +0100

    implementacija supermarketa

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,39 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class Korpa {
+    private ArrayList<Artikl> artikli;
+
+    public Korpa(){
+        artikli  = new ArrayList<Artikl>();
+    }
+
+    public  Artikl[] getArtikli() {
+        return artikli.toArray(new Artikl[0]);
+    }
+
+    public boolean dodajArtikl(Artikl a){
+        if(artikli.size()==50) return false;
+        artikli.add(a);
+        return  true;
+    }
+    public int dajUkupnuCijenuArtikala(){
+        int suma=0;
+        for(int i=0;i<artikli.size();i++){
+            suma = suma + artikli.get(i).getCijena();
+        }
+        return  suma;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl novi = null;
+        for(int i=0;i<artikli.size();i++)
+            if(kod.equals(artikli.get(i).getKod())) {
+                novi = artikli.get(i);
+                break;
+            }
+
+        artikli.remove(novi);
+        return novi;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,45 @@
 package ba.unsa.etf.rpr;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Supermarket {
+    ArrayList<Artikl> artikli;
+
+    public Supermarket(){
+        artikli  = new ArrayList<Artikl>();
+    }
+
+
+    public Artikl[] getArtikli() {
+        return artikli.toArray(new Artikl[0]);
+    }
+
+    public Artikl izbaciArtklSaKodom(String kod){
+        Artikl novi = null;
+        for(int i=0;i<artikli.size();i++)
+            if(artikli.get(i).getKod().equals(kod))
+                novi = artikli.get(i);
+
+        artikli.remove(novi);
+        return novi;
+    }
+
+    public void dodajArtikl(Artikl novi){
+
+        artikli.add(novi);
+
+    }
+
+
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl novi = null;
+        for(int i=0;i<artikli.size();i++)
+            if(artikli.get(i).getKod().equals(kod))
+                novi = artikli.get(i);
+
+        artikli.remove(novi);
+        return  novi;
+    }
 }
